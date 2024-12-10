2a97e7c044fecebbd01d9395c947148ff7591592
GiorgiaSecchi <secchigrg@gmail.com>
2024-12-10 15:39:32 +0100

    add about

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={HomePage} />
+        <Route path="/about" Component={AboutPage} />
       </Routes>
     </BrowserRouter>
   );
