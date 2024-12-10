1d27b43e79c46b89939d6c615717936b2a7a41c9
GiorgiaSecchi <secchigrg@gmail.com>
2024-12-10 16:35:18 +0100

    mod PostsPage

---
+++ src/components/Cards.jsx
@@ -0,0 +1,17 @@
+export default function Cards() {
+  return (
+    <div className="card mb-3">
+      <img src="..." className="card-img-top " alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Post title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a to="#" className="btn btn-primary">
+          Leggi di più
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,7 +1,15 @@
+import Cards from "../components/Cards";
+
 export default function PostsPage() {
   return (
     <div className="container py-5">
       <h1>Posts</h1>
+
+      <div className="py-5">
+        <Cards />
+        <Cards />
+        <Cards />
+      </div>
     </div>
   );
 }
