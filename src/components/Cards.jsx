a28cae2700e39c9cfc7fd03f3dff413e9c662db8
GiorgiaSecchi <secchigrg@gmail.com>
2024-12-11 19:12:00 +0100

    add Cards (ShowPosts)

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,51 @@
-export default function Cards() {
+const apiUrl = import.meta.env.VITE_APP_URL;
+
+export default function Cards({ titolo, immagine, tags, descrizione }) {
   return (
-    <div className="card mb-3">
-      <img src="..." className="card-img-top " alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Post title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+    <div class="card mb-3">
+      <img src={apiUrl + immagine} class="card-img-top" alt={titolo} />
+      <div class="card-body">
+        <h5 class="card-title">{titolo}</h5>
+        <p class="card-text">{descrizione}</p>
+        <p class="card-text">
+          <small class="text-body-secondary">{tags}</small>
         </p>
-        <a to="#" className="btn btn-primary">
-          Leggi di più
-        </a>
       </div>
     </div>
   );
+}
+
+{
+  /* <div classNameName="card mb-3">
+      <img src={apiUrl + immagine} classNameName="card-img-top " alt={titolo} />
+      <div classNameName="card-body">
+        <h5 classNameName="card-title">{titolo}</h5>
+        <p classNameName="card-text">{descrizione}</p>
+        <p classNameName="card-text">{tags.join(", ")}</p>
+      </div>
+    </div> */
+}
+
+{
+  /* 
+<div className="card mb-3">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img
+            src={apiUrl + immagine}
+            className="img-fluid rounded-start"
+            alt={titolo}
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{titolo}</h5>
+            <p className="card-text">{descrizione}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">{tags}</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div> */
 }
--- src/pages/posts/ShowPosts.jsx
+++ src/pages/posts/ShowPosts.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Cards from "../../components/Cards";
 
 export default function ShowPosts() {
   const apiUrl = import.meta.env.VITE_APP_URL;
@@ -18,6 +19,11 @@ export default function ShowPosts() {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        // Aggiorna lo setPost con i dati ricevuti
+        setPost(data);
+      })
+      .catch((error) => {
+        console.error("Post not found", error);
       });
   };
 
@@ -25,7 +31,33 @@ export default function ShowPosts() {
     <div className="container py-5">
       <h1>Dettaglio Post</h1>
 
-      <div className="py-5"></div>
+      <div className="py-5">
+        {post ? (
+          <Cards
+            titolo={post.titolo}
+            immagine={post.immagine}
+            descrizione={post.contenuto}
+            tags={post.tags.map((tag, index) => (
+              <span key={index} className="badge text-bg-secondary me-1">
+                {tag}
+              </span>
+            ))}
+          />
+        ) : (
+          <p>Caricamento in corso...</p>
+        )}
+      </div>
     </div>
   );
 }
+
+// (
+//     <Cards
+//       titolo={post.titolo}
+//       immagine={post.immagine}
+//       tags={post.tags}
+//       contenuto={post.contenuto}
+//     />
+//   ) : (
+//     <p>Caricamento in corso...</p>
+//   )
