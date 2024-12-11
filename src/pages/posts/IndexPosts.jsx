34b8e679b0330e031b3911b000c44c5979d67199
GiorgiaSecchi <secchigrg@gmail.com>
2024-12-11 17:08:04 +0100

    map posts list

--- src/pages/posts/IndexPosts.jsx
+++ src/pages/posts/IndexPosts.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function IndexPosts() {
+  const [posts, setPosts] = useState([]);
   const apiUrl = import.meta.env.VITE_APP_URL;
   console.log("apiUrl:" + apiUrl);
 
@@ -8,11 +9,27 @@ export default function IndexPosts() {
     fetchPosts();
   }, []);
 
+  //   const fetchPosts = () => {
+  //     fetch(apiUrl + "/posts")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //       });
+  //   };
+
   const fetchPosts = () => {
     fetch(apiUrl + "/posts")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        const postsData = data.map((post) => ({
+          id: post.id,
+          titolo: post.titolo,
+          immagine: post.immagine,
+          tags: post.tags,
+        }));
+
+        console.log(postsData);
+        setPosts(postsData);
       });
   };
 
@@ -20,28 +37,38 @@ export default function IndexPosts() {
     <div className="container py-5">
       <h1>Posts</h1>
 
-      <div className="py-5">
-        <table className="table table-hover table-striped">
-          <thead>
-            <tr>
-              <th scope="col">Immagine</th>
-              <th scope="col">Titolo</th>
-              <th scope="col">Tags</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>
-                <a href="#">Mostra</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <hr />
+
+      {posts.length > 0 ? (
+        <div className="py-4">
+          <table className="table table-hover table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Immagine</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Tags</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map((post) => (
+                <tr key={post.id}>
+                  <td>
+                    <img src={apiUrl + post.immagine} alt="IMG" width="70" />
+                  </td>
+                  <td>{post.titolo}</td>
+                  <td>{post.tags}</td>
+                  <td>
+                    <a href="#">Mostra</a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <h5 className="mt-5">Non ci sono posts!</h5>
+      )}
     </div>
   );
 }
